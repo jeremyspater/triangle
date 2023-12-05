cff55a26af9893e37aa5ec06612e19e044109647
Jeremy Spater <jeremyspater@gmail.com>
2023-12-05 14:23:38 +0100

    more comments and test cases

--- triangle_JS.py
+++ triangle_JS.py
@@ -1,3 +1,5 @@
+#Program to classify triangle, based on side lengths
+
 from typing import List
 import math #for 'isclose'
 
@@ -17,10 +19,10 @@ def tri_clas(sides: List[float]):
         raise ValueError("Input must correspond to a valid triangle: one length cannot exceed sum of other two")
     
     #Logic for classifying triangle:
-    if math.isclose(sides[0], sides[1]) and math.isclose(sides[1], sides[2]):
+    if math.isclose(sides[0], sides[1]) and math.isclose(sides[1], sides[2]): 
         return('Equilateral')
     
-    elif math.isclose(sides[0], sides[1]) or math.isclose(sides[1], sides[2]) or math.isclose(sides[0], sides[2]): #using isclose
+    elif math.isclose(sides[0], sides[1]) or math.isclose(sides[1], sides[2]) or math.isclose(sides[0], sides[2]): 
         return('Isoceles')
     
     else:
@@ -37,13 +39,23 @@ print(tri_clas([1,1,1]))
 
 #Illegitimate cases:
 #print(tri_clas(['a','b','c'])) 
+#print(tri_clas(['a', 2, 3])) 
 #print(tri_clas([1,2,3,4]))
 #print(tri_clas([1, 1, -1]))
+#print(tri_clas([1, 1, 0]))
 #print(tri_clas([1, 2, 1]))
 #print(tri_clas([3, 1, 1]))
+#print(tri_clas([1, 1, 3]))
+#print(tri_clas([1E99, 1E99, 3E99]))
+#print(tri_clas([1, 1, 1+1j])) 
 
-#Interesting cases:
+#Alternative number specifications:
 #print(tri_clas([1.5, 3/2, 1.5]))
-#print(tri_clas([0.3, 0.1+0.2, 0.3])) #'isclose' resolves floating-point rounding error here
+#print(tri_clas([0.3, 0.1+0.2, 0.3])) #'isclose' prevents floating-point rounding error here
 #print(tri_clas([1E99, 1E99, 1E99]))
-#print(tri_clas([1E99, 1E99, 3E99])) #this throws an error, as it should
+#print(tri_clas([1.,1.,1.]))
+#print(tri_clas([1.,1,1]))
+#print(tri_clas([True,1,1]))
+
+
+
