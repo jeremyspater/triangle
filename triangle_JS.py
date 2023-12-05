ffe12b2b6e1572c86cac85433c3bbf7fae4521d1
Jeremy Spater <jeremyspater@gmail.com>
2023-12-05 12:14:05 +0100

    Add comments to triangle_JS.py

--- triangle_JS.py
+++ triangle_JS.py
@@ -1,23 +1,26 @@
 from typing import List
 import math #for 'isclose'
 
-def tri_clas(sides: List[float]): #
+def tri_clas(sides: List[float]):
+
+    #Input validation:
     if not isinstance(sides, list):
         raise ValueError("Input must be a list of numbers.")
     
     if not len(sides) == 3:
         raise ValueError("Input must contain three numbers.")
     
-    if not (sides[0] + sides[1] > sides[2]) and (sides[1] + sides[2] > sides[0]) and (sides[0] + sides[2] > sides[1]):
+    if not (sides[0] + sides[1] > sides[2]) and (sides[1] + sides[2] > sides[0]) and (sides[0] + sides[2] > sides[1]): #Condition for a triangle
         raise ValueError("Input must correspond to a valid triangle: one length cannot exceed sum of other two")
     
     if not all(isinstance(num, (int, float)) and num > 0 for num in sides):
         raise ValueError("All elements in the list must be positive numbers (integer or float).")
 
-    if math.isclose(sides[0], sides[1]) and math.isclose(sides[1], sides[2]): 
+    #Logic for classifying triangle:
+    if math.isclose(sides[0], sides[1]) and math.isclose(sides[1], sides[2]):
         return('Equilateral')
     
-    elif math.isclose(sides[0], sides[1]) or math.isclose(sides[1], sides[2]) or math.isclose(sides[0], sides[2]): #using isclose    
+    elif math.isclose(sides[0], sides[1]) or math.isclose(sides[1], sides[2]) or math.isclose(sides[0], sides[2]): #using isclose
         return('Isoceles')
     
     else:
